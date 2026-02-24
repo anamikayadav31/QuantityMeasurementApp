a89e1aade52cf635839b662329a1f30b07560eba
Anamika Yadav <141858575+anamikayadav31@users.noreply.github.com>
2026-02-24 15:58:17 +0530

    [Anamika] added UC3 Generic Length

--- QuantityMeasurementApp/Program.cs
+++ QuantityMeasurementApp/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using QuantityMeasurementApp.Models;
 using QuantityMeasurementApp.Services;
 
@@ -33,22 +33,31 @@ namespace QuantityMeasurementApp
                 Console.Write("Enter second value: ");
                 double input2 = Convert.ToDouble(Console.ReadLine());
 
-                // Convert user choice to LengthUnit enum
-                LengthUnit unit1 = unitChoice1 == 1 ? LengthUnit.FEET :
-                                   unitChoice1 == 2 ? LengthUnit.INCH :
-                                   throw new ArgumentException("Invalid unit selection for first value.");
+                // Convert user choice to LengthUnit enum using if-else
+                LengthUnit unit1;
+                if (unitChoice1 == 1)
+                    unit1 = LengthUnit.FEET;
+                else if (unitChoice1 == 2)
+                    unit1 = LengthUnit.INCH;
+                else
+                    throw new ArgumentException("Invalid unit selection for first value.");
 
-                LengthUnit unit2 = unitChoice2 == 1 ? LengthUnit.FEET :
-                                   unitChoice2 == 2 ? LengthUnit.INCH :
-                                   throw new ArgumentException("Invalid unit selection for second value.");
+                LengthUnit unit2;
+                if (unitChoice2 == 1)
+                    unit2 = LengthUnit.FEET;
+                else if (unitChoice2 == 2)
+                    unit2 = LengthUnit.INCH;
+                else
+                    throw new ArgumentException("Invalid unit selection for second value.");
 
                 // Create Quantity objects
                 QuantityLength quantity1 = new QuantityLength(input1, unit1);
                 QuantityLength quantity2 = new QuantityLength(input2, unit2);
 
-                // Compare using single service method
+                // Compare using service method
                 bool result = service.AreEqual(quantity1, quantity2);
 
+                // Display result
                 Console.WriteLine("\nComparing...");
                 Console.WriteLine($"First:  {quantity1}");
                 Console.WriteLine($"Second: {quantity2}");
